0a4f4b4b52e055cb99a3c82fc8003e0b158c1413
Devin Neal <devin@eulertour.com>
2024-07-14 16:04:28 -0400

    Use old storageDeploy script

--- storageDeploy/src/index.ts
+++ storageDeploy/src/index.ts
@@ -1,82 +1,100 @@
-import { type AppOptions, initializeApp } from 'firebase-admin/app';
-import { dirname, join, basename, relative } from 'node:path';
-import { getStorage } from 'firebase-admin/storage';
-import { setTimeout } from 'node:timers/promises';
-import { readdir } from 'node:fs/promises';
-import { fileURLToPath } from 'node:url';
+import { initializeApp } from "firebase-admin/app";
+import { getStorage } from "firebase-admin/storage";
+import { readdirSync, lstatSync } from "fs";
+import path from "path";
+import axios from "axios";
 
-const MODE = process.argv.join(' ').includes('--production')
-	? 'production'
-	: process.env['FIREBASE_STORAGE_EMULATOR_HOST']
-	? 'emulator'
-	: 'staging';
+const isReady = async () => {
+  try {
+    await axios.get("http://localhost:9199");
+  } catch (error: any) {
+    return error.code !== "ECONNREFUSED";
+  }
+  return true;
+};
 
-let ready = false;
+async function* getFiles(
+  directory = ".",
+  ignoreRegexes: Array<RegExp>,
+): AsyncGenerator<string> {
+  for (const file of readdirSync(directory)) {
+    const fullPath = path.join(directory, file);
+    const stats = lstatSync(fullPath);
 
-while (!ready) {
-	const response = await fetch('http://localhost:9199').catch(() => null);
-	ready = response?.status == 501;
+    if (
+      stats.isDirectory() &&
+      !stats.isSymbolicLink() &&
+      ignoreRegexes.every((re) => !fullPath.match(re))
+    ) {
+      yield* getFiles(fullPath, ignoreRegexes);
+    }
 
-	console.log('Waiting for Firebase to come online...');
-	await setTimeout(500);
+    if (stats.isFile()) {
+      yield fullPath;
+    }
+  }
 }
-
-console.log('Firebase online');
-
-const config: AppOptions =
-	MODE == 'staging' || MODE == 'emulator'
-		? {
-				projectId: 'euler-studio-staging',
-				storageBucket: 'euler-studio-staging.appspot.com',
-		  }
-		: {
-				projectId: 'euler-studio',
-				storageBucket: 'euler-studio.appspot.com',
-		  };
-
-const ignoreRegexes = [/.*node_modules.*/, /.*examples\/dist.*/];
-const currentDirectory = dirname(fileURLToPath(import.meta.url));
-const app = initializeApp(config);
-const bucket = getStorage(app).bucket();
-
-await uploadDirectory(
-	join(currentDirectory, '../../build'),
-	bucket,
-	ignoreRegexes,
-);
-
-await uploadDirectory(
-	join(currentDirectory, '../../src'),
-	bucket,
-	ignoreRegexes,
-);
-
-await uploadDirectory(
-	join(currentDirectory, '../../examples'),
-	bucket,
-	ignoreRegexes,
-);
-
-console.log('Done');
-
-type Bucket = typeof bucket;
 
 async function uploadDirectory(
-	directory: string,
-	bucket: Bucket,
-	ignoreRegexes: RegExp[],
+  directory: string,
+  bucket: any,
+  ignoreRegexes: Array<RegExp>,
 ) {
-	const files = await readdir(directory, {
-		recursive: true,
-		withFileTypes: true,
-	});
+  for await (const filePath of getFiles(directory, ignoreRegexes)) {
+    try {
+      const source = filePath;
+      const destination = path.join(
+        "studio.js",
+        path.basename(directory),
+        path.relative(directory, filePath),
+      );
 
-	for (const file of files) {
-		if (ignoreRegexes.some((pattern) => file.name.match(pattern))) continue;
-		if (file.isDirectory()) continue;
-
-		const destination = join('studio.js', basename(directory), file.name);
-
-		await bucket.upload(join(file.parentPath, file.name), { destination });
-	}
+      await bucket.upload(source, { destination });
+    } catch (e) {
+      console.error(`Error uploading ${filePath}:`, e);
+    }
+  }
 }
+
+let appConfig;
+let mode;
+if (
+  process.argv.length < 3 ||
+  (process.argv.length >= 3 && process.argv[2] === "--staging")
+) {
+  if (process.argv.length < 3) {
+    mode = "emulator";
+  } else {
+    mode = "staging";
+  }
+  appConfig = {
+    projectId: "euler-studio-staging",
+    storageBucket: "euler-studio-staging.appspot.com",
+  };
+} else if (process.argv.length >= 3 && process.argv[2] === "--production") {
+  mode = "production";
+  appConfig = {
+    projectId: "euler-studio",
+    storageBucket: "euler-studio.appspot.com",
+  };
+}
+
+const app = initializeApp(appConfig);
+const bucket = getStorage(app).bucket();
+const ignoreRegexes = [/.*node_modules.*/, /.*examples\/dist.*/];
+
+let ready = await isReady();
+while (!ready) {
+  const readyPromise = new Promise<boolean>((resolve) => {
+    setTimeout(() => {
+      const ready = isReady();
+      resolve(ready);
+    }, 1000);
+  });
+  ready = await readyPromise;
+}
+
+uploadDirectory("../build", bucket, ignoreRegexes);
+uploadDirectory("../src", bucket, ignoreRegexes);
+uploadDirectory("../examples", bucket, ignoreRegexes);
+console.log(`Updated ${mode} storage`);
