7019dfd012b258e4847b877447c06e593c1e11fc
Devin Neal <devin@eulertour.com>
2024-10-12 22:19:29 -0400

    Update line test

--- test/test.ts
+++ test/test.ts
@@ -9,10 +9,17 @@ describe("Line", () => {
 
   describe("attributes", () => {
     it("should have a start and end attribute", () => {
+      const attributes = line.getAttributes();
+      assert.containsAllKeys(attributes, ["start", "end"]);
+    });
+
+    it("should have the correct start attribute", () => {
       const start = line.start;
       const startDifference = new THREE.Vector3(0, 0, 0).sub(start).length();
       assert.isAtMost(startDifference, 0.001);
+    });
 
+    it("should have the corrent end attribute", () => {
       const end = line.end;
       const endDifference = new THREE.Vector3(1, 1, 1).sub(end).length();
       assert.isAtMost(endDifference, 0.001);
