4f8fc09f06d65a74d0b561fe3a392b2624cad7fc
Devin Neal <devin@eulertour.com>
2024-12-18 22:35:45 -0500

    Install firebase-admin, add importAttributesKey

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,19 +1,20 @@
-import commonjs from '@rollup/plugin-commonjs';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import ts from 'rollup-plugin-ts';
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import ts from "rollup-plugin-ts";
 
 export default [
-	{
-		input: 'src/index.ts',
-		output: {
-			file: 'build/bundle.js',
-			format: 'es',
-		},
-		plugins: [
-			nodeResolve({ extensions: ['.js', '.ts'] }),
-			ts(),
-			commonjs(),
-			// terser(),
-		],
-	},
+  {
+    input: "src/index.ts",
+    output: {
+      file: "build/bundle.js",
+      format: "es",
+      importAttributesKey: "with",
+    },
+    plugins: [
+      nodeResolve({ extensions: [".js", ".ts"] }),
+      ts(),
+      commonjs(),
+      // terser(),
+    ],
+  },
 ];
