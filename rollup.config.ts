e4189f3e70e9dacf2caddf56969393bc39cccd6a
Devin Neal <devin@eulertour.com>
2024-12-20 02:15:03 +0000

    Add preview script

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,11 +2,16 @@ import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import ts from "rollup-plugin-ts";
 
+let outputDir = "build";
+if (process.env.OUTPUT_DIR) {
+  outputDir = process.env.OUTPUT_DIR;
+}
+
 export default [
   {
     input: "src/index.ts",
     output: {
-      file: "build/bundle.js",
+      file: `${outputDir}/bundle.js`,
       format: "es",
       importAttributesKey: "with",
     },
