d5da37348e6e53b38022ecfb3314ee4f95f61d9a
kturkal <166247156+kturkal@users.noreply.github.com>
2025-01-21 11:05:51 -0500

    Set Opacity in it's own file

--- src/animations/animation.ts
+++ src/animations/animation.ts
@@ -7,6 +7,7 @@ import Draw from "./draw.js";
 import Erase from "./erase.js";
 import SetScale from "./setscale.js";
 import FadeIn from "./fadein.js";
+import SetOpacity from "./setopacity.js";
 
 const sigmoid = (x) => 1 / (1 + Math.exp(-x));
 const smooth = (t) => {
@@ -222,58 +223,6 @@ class FadeOut extends Animation {
   }
 }
 
-class SetOpacity extends Animation {
-  initialOpacity = new Map();
-
-  constructor(
-    objectOrFunc,
-    public targetOpacity,
-    public config?,
-  ) {
-    let family = true;
-    if (config && config.family === false) {
-      family = false;
-    }
-
-    super(
-      (elapsedTime, _deltaTime) => {
-        if (family) {
-          this.object.traverse((child) => {
-            if (child instanceof THREE.Mesh) {
-              if (!this.initialOpacity.has(child)) {
-                console.error("Unknown child");
-              }
-              child.material.opacity = THREE.MathUtils.lerp(
-                this.initialOpacity.get(child),
-                this.targetOpacity,
-                elapsedTime,
-              );
-            }
-          });
-        } else {
-          [this.object.stroke, this.object.fill].forEach((mesh) => {
-            if (!mesh) return;
-            mesh.material.opacity = THREE.MathUtils.lerp(
-              this.initialOpacity.get(mesh),
-              this.targetOpacity,
-              elapsedTime,
-            );
-          });
-        }
-      },
-      { object: objectOrFunc, ...config },
-    );
-  }
-
-  setUp() {
-    super.setUp();
-    this.object.traverse((child: THREE.Object3D) => {
-      if (child instanceof THREE.Mesh) {
-        this.initialOpacity.set(child, child.material.opacity);
-      }
-    });
-  }
-}
 
 class Wait extends Animation {
   constructor(config?) {
---
+++ src/animations/setopacity.ts
@@ -0,0 +1,56 @@
+import { Animation } from "./animation.js";
+import * as THREE from "three";
+
+export default class SetOpacity extends Animation {
+    initialOpacity = new Map();
+  
+    constructor(
+      objectOrFunc,
+      public targetOpacity,
+      public config?,
+    ) {
+      let family = true;
+      if (config && config.family === false) {
+        family = false;
+      }
+  
+      super(
+        (elapsedTime, _deltaTime) => {
+          if (family) {
+            this.object.traverse((child) => {
+              if (child instanceof THREE.Mesh) {
+                if (!this.initialOpacity.has(child)) {
+                  console.error("Unknown child");
+                }
+                child.material.opacity = THREE.MathUtils.lerp(
+                  this.initialOpacity.get(child),
+                  this.targetOpacity,
+                  elapsedTime,
+                );
+              }
+            });
+          } else {
+            [this.object.stroke, this.object.fill].forEach((mesh) => {
+              if (!mesh) return;
+              mesh.material.opacity = THREE.MathUtils.lerp(
+                this.initialOpacity.get(mesh),
+                this.targetOpacity,
+                elapsedTime,
+              );
+            });
+          }
+        },
+        { object: objectOrFunc, ...config },
+      );
+    }
+  
+    setUp() {
+      super.setUp();
+      this.object.traverse((child: THREE.Object3D) => {
+        if (child instanceof THREE.Mesh) {
+          this.initialOpacity.set(child, child.material.opacity);
+        }
+      });
+    }
+  }
+  
