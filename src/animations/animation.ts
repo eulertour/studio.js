b571edb3f3ca9a00b82f431f3657a334d2dbd71b
kturkal <166247156+kturkal@users.noreply.github.com>
2025-01-21 11:08:52 -0500

    fadeout in it's own file

--- src/animations/animation.ts
+++ src/animations/animation.ts
@@ -1,4 +1,3 @@
-import * as THREE from "three";
 import { clamp } from "../utils.js";
 import Shift from "./shift.js";
 import MoveTo from "./Moveto.js";
@@ -8,6 +7,7 @@ import Erase from "./erase.js";
 import SetScale from "./setscale.js";
 import FadeIn from "./fadein.js";
 import SetOpacity from "./setopacity.js";
+import FadeOut from "./fadeout.js";
 
 const sigmoid = (x) => 1 / (1 + Math.exp(-x));
 const smooth = (t) => {
@@ -148,78 +148,6 @@ class Animation {
     } else {
       this.afterFunc = after;
     }
-  }
-}
-
-
-
-
-class FadeOut extends Animation {
-  initialOpacity = new Map();
-
-  constructor(
-    objectOrFunc,
-    public config?,
-  ) {
-    let family = true;
-    if (config && config.family === false) {
-      family = false;
-    }
-
-    super(
-      (elapsedTime, _deltaTime) => {
-        if (family) {
-          this.object.traverse((child) => {
-            if (child instanceof THREE.Mesh) {
-              if (!this.initialOpacity.has(child)) {
-                console.error("Unknown child");
-              }
-              child.material.opacity = THREE.MathUtils.lerp(
-                this.initialOpacity.get(child),
-                0,
-                elapsedTime,
-              );
-            }
-          });
-        } else {
-          [this.object.stroke, this.object.fill].forEach((mesh) => {
-            if (!mesh) return;
-            mesh.material.opacity = THREE.MathUtils.lerp(
-              this.initialOpacity.get(mesh),
-              0,
-              elapsedTime,
-            );
-          });
-        }
-      },
-      { object: objectOrFunc, hide: true, ...config },
-    );
-  }
-
-  setUp() {
-    super.setUp();
-    this.object.traverse((child: THREE.Object3D) => {
-      if (child instanceof THREE.Mesh) {
-        this.initialOpacity.set(child, child.material.opacity);
-      }
-    });
-  }
-
-  tearDown() {
-    if (this.config?.remove) {
-      this.object.parent.remove(this.object);
-    }
-    if (this.config?.restore) {
-      this.object.traverse((child) => {
-        if (child instanceof THREE.Mesh) {
-          if (!this.initialOpacity.has(child)) {
-            console.error("Unknown child");
-          }
-          child.material.opacity = this.initialOpacity.get(child);
-        }
-      });
-    }
-    super.tearDown();
   }
 }
 
