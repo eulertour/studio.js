0ab463148bba1a9127801b95aa1f5a58a08fc29f
kturkal <166247156+kturkal@users.noreply.github.com>
2025-01-31 15:12:36 -0500

    Draw errors fixed

--- src/animation/Draw.ts
+++ src/animation/Draw.ts
@@ -1,12 +1,12 @@
 import { Animation } from "./Animation.js";
-
+import * as THREE from "three";
 export default class Draw extends Animation {
-    constructor(object, config?) {
+    constructor(object: THREE.Object3D, config?: any) {
       super(
         (elapsedTime) => {
-          this.object.traverse((child) => {
-            if (child.stroke) {
-              child.stroke.material.uniforms.drawRange.value.y = elapsedTime;
+          this.object.traverse((child: THREE.Object3D) => {
+            if (child instanceof THREE.Line) {
+              child.material.uniforms.drawRange.value.y = elapsedTime;
             }
           });
         },
