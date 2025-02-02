541d0d80b6476f132d53be79d283f5c02475bd26
kturkal <166247156+kturkal@users.noreply.github.com>
2025-02-01 22:06:37 -0500

    Make polygon automatically close

--- src/geometry/Polygon.ts
+++ src/geometry/Polygon.ts
@@ -1,5 +1,6 @@
 import Shape, { Style } from "./Shape.js";
 import { THREE } from "../index.js";
+import { ERROR_THRESHOLD } from "../constants.js";
 
 export type PolygonAttributes = {
   points: Array<THREE.Vector3>;
@@ -11,9 +12,26 @@ export type PolygonAttributes = {
  *
  * @example polygon.ts
  */
-
 export default class Polygon extends Shape {
-  constructor(points: Array<THREE.Vector3>, config: Style = {}) {
+  constructor(inputPoints: Array<THREE.Vector3>, config: Style = {}) {
+    let points = [...inputPoints];
+
+    if (points.length < 3) {
+      throw new Error("Polygon must be called with at least three points");
+    }
+
+    // Ensure the polygon is closed by adding the first point to
+    // the end if necessary.
+    const firstPoint = points[0];
+    const lastPoint = points[points.length - 1];
+    if (firstPoint === undefined || lastPoint === undefined) {
+      throw new Error("firstPoint or lastPoint is undefined");
+    }
+    const firstToLastDistance = firstPoint.distanceTo(lastPoint);
+    if (firstToLastDistance > ERROR_THRESHOLD) {
+      points.push(firstPoint.clone());
+    }
+
     super(points, { ...Polygon.defaultConfig(), ...config });
     this.curveEndIndices = [];
     for (let i = 0; i < points.length - 1; i++) {
