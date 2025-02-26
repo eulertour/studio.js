2df88aa7489d9be258eae9cd1fbb0e002969a267
Devin Neal <devin@eulertour.com>
2025-02-26 13:33:21 -0500

    Move dev scene into userScene/main.ts

---
+++ userScene/main.ts
@@ -0,0 +1,44 @@
+import {
+  Geometry,
+  Animation,
+  StudioScene,
+  AnimationRepresentation,
+} from "../src/index.js";
+import * as THREE from "three";
+
+export default class Scene implements StudioScene {
+  animations?: Array<AnimationRepresentation>;
+
+  constructor(
+    public scene: THREE.Scene,
+    public camera: THREE.OrthographicCamera,
+    public renderer: THREE.WebGLRenderer,
+  ) {
+    const triangle = new Geometry.Polygon(
+      [
+        new THREE.Vector3(-1, -1, 0),
+        new THREE.Vector3(1, 1, 0),
+        new THREE.Vector3(1, -1, 0),
+      ],
+      {
+        strokeWidth: 4,
+        fillColor: new THREE.Color("red"),
+        fillOpacity: 0.5,
+        strokeColor: new THREE.Color("red"),
+      },
+    );
+    scene.add(triangle);
+
+    this.animations = [
+      new Animation.Wait(),
+
+      new Animation.Animation((t, _) => {
+        triangle.reshape([
+          new THREE.Vector3(-1, -1, 0),
+          new THREE.Vector3(1 + t, 1, 0),
+          new THREE.Vector3(1, -1, 0),
+        ]);
+      }),
+    ];
+  }
+}
