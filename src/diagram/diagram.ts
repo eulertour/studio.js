0c04428296204ea54767e3c0adce193b58295ce3
kturkal <166247156+kturkal@users.noreply.github.com>
2025-03-16 13:34:26 -0400

    Congruent angle in it's own file

--- src/diagram/diagram.ts
+++ src/diagram/diagram.ts
@@ -12,36 +12,6 @@ interface IndicatorConfig {
 
 
 
-
-
-class CongruentAngle extends THREE.Group {
-  constructor(
-    arcs: number,
-    point1: THREE.Vector3,
-    point2: THREE.Vector3,
-    point3: THREE.Vector3,
-    public config: Geometry.Style & {
-      minRadius?: number;
-      spacing?: number;
-    } = {},
-  ) {
-    config = {
-      minRadius: 0.4,
-      spacing: 0.15,
-      ...config,
-    };
-
-    super();
-    for (let i = 0; i < arcs; i++) {
-      const arc = new Angle(point1, point2, point3, {
-        radius: config.minRadius + i * config.spacing,
-        ...config,
-      });
-      this.add(arc);
-    }
-  }
-}
-
 class RightAngle extends Geometry.Polyline {
   constructor(
     point1: THREE.Vector3,
