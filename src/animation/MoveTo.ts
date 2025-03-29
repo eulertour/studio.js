c50e486cf481df9a282a0a7cd4f5d1ac0d395062
kturkal <166247156+kturkal@users.noreply.github.com>
2025-03-29 12:57:02 -0400

    Reconfigured Moveto

--- src/animation/MoveTo.ts
+++ src/animation/MoveTo.ts
@@ -3,12 +3,12 @@ import * as THREE from "three";
 import { getBoundingBoxCenter } from "../utils.js";
 
 export default class MoveTo extends Animation {
-  public start: THREE.Vector3;
-  public displacement: THREE.Vector3;
+  public start!: THREE.Vector3;
+  public displacement!: THREE.Vector3;
 
   constructor(
     public obj: THREE.Object3D,
-    public target: THREE.Vector3,
+    public target: THREE.Object3D | THREE.Vector3,
     config?: any,
   ) {
     super(
@@ -27,8 +27,32 @@ export default class MoveTo extends Animation {
 
     const final = new THREE.Vector3();
     const initial = new THREE.Vector3();
-    this.obj.parent.worldToLocal(getBoundingBoxCenter(this.target, final));
-    this.obj.parent.worldToLocal(getBoundingBoxCenter(this.obj, initial));
+    
+    if (this.target instanceof THREE.Vector3) {
+      final.copy(this.target);
+    } else {
+      getBoundingBoxCenter(this.target, final);
+    }
+    
+    getBoundingBoxCenter(this.obj, initial);
+    this.obj.parent?.worldToLocal(final);
+    this.obj.parent?.worldToLocal(initial);
     this.displacement = new THREE.Vector3().subVectors(final, initial);
   }
 }
+/* 
+
+Example Usage:
+
+ new Animation.MoveTo(triangle, new THREE.Vector3(4, 1, 0));
+
+ new Animation.MoveTo(triangle, diagram.square);
+
+ new Animation.MoveTo(triangle, diagram.square.position);
+
+
+
+
+
+
+*/
