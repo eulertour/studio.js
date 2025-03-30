aa9232b5a03d1736302fcfa963da1ac855b450aa
Devin Neal <devin@eulertour.com>
2025-03-29 22:50:55 -0400

    Restore main.ts

--- userScene/main.ts
+++ userScene/main.ts
@@ -14,35 +14,30 @@ export default class Scene implements StudioScene {
     public camera: THREE.OrthographicCamera,
     public renderer: THREE.WebGLRenderer,
   ) {
-    const xOffset = 5;
-    const yOffset = 1.25;
-    const numSquares = 5;
-    const squares = [];
-    for (let i = 0; i < numSquares; i++) {
-      const square = new Geometry.Square(1, {
-        position: new THREE.Vector3(
-          -xOffset / 2,
-          (yOffset * (numSquares - 1)) / 2 - i * yOffset,
-          0,
-        ),
-      });
-      scene.add(square);
-      squares.push(square);
-    }
+    const triangle = new Geometry.Polygon(
+      [
+        new THREE.Vector3(-1, -1, 0),
+        new THREE.Vector3(1, 1, 0),
+        new THREE.Vector3(1, -1, 0),
+      ],
+      {
+        strokeWidth: 4,
+        fillColor: new THREE.Color("red"),
+        fillOpacity: 0.5,
+        strokeColor: new THREE.Color("red"),
+      },
+    );
+    scene.add(triangle);
 
     this.animations = [
-      new Animation.Stagger(
-        [
-          new Animation.Shift(squares[0], new THREE.Vector3(xOffset, 0, 0)),
-          new Animation.Shift(squares[1], new THREE.Vector3(xOffset, 0, 0)),
-          new Animation.Shift(squares[2], new THREE.Vector3(xOffset, 0, 0)),
-          new Animation.Shift(squares[3], new THREE.Vector3(xOffset, 0, 0)),
-          new Animation.Shift(squares[4], new THREE.Vector3(xOffset, 0, 0)),
-        ],
-        {
-          staggerDuration: 0.2,
-        },
-      ),
+      new Animation.Wait(),
+      new Animation.Animation((t, _) => {
+        triangle.reshape([
+          new THREE.Vector3(-1, -1, 0),
+          new THREE.Vector3(1 + t, 1, 0),
+          new THREE.Vector3(1, -1, 0),
+        ]);
+      }),
     ];
   }
 }
