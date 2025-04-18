086bc8d6130196e60511761832e04d9aa867b717
Devin Neal <devin@eulertour.com>
2025-04-18 02:24:58 -0400

    switch A/B + formatting

--- src/geometry/Ellipse.ts
+++ src/geometry/Ellipse.ts
@@ -3,80 +3,66 @@ import Shape, { Style } from "./Shape.js";
 import * as THREE from "three";
 
 export type EllipseAttributes = {
-    radiusA: number;
-    radiusB: number;
+  radiusA: number;
+  radiusB: number;
 };
 
-/**
- * Ellipse
- *
- * @example ellipse.ts
- */
-
 export default class Ellipse extends Shape {
-    constructor(
-        public radiusA = 1,
-        public radiusB = 2,
-        config: Style = {},
-    ) {
-        const angle = 2 * Math.PI;
-        let points = [];
+  constructor(
+    public radiusA = 2,
+    public radiusB = 1,
+    config: Style = {},
+  ) {
+    const angle = 2 * Math.PI;
+    let points = [];
 
-        for (let i = 0; i <= angle + ERROR_THRESHOLD; i += angle / 50) {
-            const angle = i;
-            const x = radiusB * Math.cos(angle);
-            const y = radiusA * Math.sin(angle);
-            points.push(new THREE.Vector3(x, y, 0));
-        }
-        
-        super(points, {
-             ...Ellipse.defaultConfig(),
-            ...config 
-        });
+    for (let i = 0; i <= angle + ERROR_THRESHOLD; i += angle / 50) {
+      const angle = i;
+      const x = radiusA * Math.cos(angle);
+      const y = radiusB * Math.sin(angle);
+      points.push(new THREE.Vector3(x, y, 0));
     }
 
-    reshape(radiusA: number, radiusB: number, config = {}) {
-        this.radiusA = radiusA;
-        this.radiusB = radiusB;
-        this.copyStrokeAndFill(new Ellipse(radiusA, radiusB, config));
-    }   
+    super(points, {
+      ...Ellipse.defaultConfig(),
+      ...config,
+    });
+  }
 
-    getCloneAttributes() {  
-        return [this.radiusA, this.radiusB];
-    }
+  reshape(radiusA: number, radiusB: number, config = {}) {
+    this.radiusA = radiusA;
+    this.radiusB = radiusB;
+    this.copyStrokeAndFill(new Ellipse(radiusA, radiusB, config));
+  }
 
-    getAttributes(): EllipseAttributes {
-        return {
-            radiusA: this.radiusA,
-            radiusB: this.radiusB,
-        };
-    }
+  getCloneAttributes() {
+    return [this.radiusA, this.radiusB];
+  }
 
-    static fromAttributes(attributes: EllipseAttributes): Ellipse {
-        const { radiusA, radiusB } = attributes;
-        return new Ellipse(radiusA, radiusB);
-    }
+  getAttributes(): EllipseAttributes {
+    return {
+      radiusA: this.radiusA,
+      radiusB: this.radiusB,
+    };
+  }
 
-    get attributeData() {
-        return [
-            {
-                attribute: "radiusA",
-                type: "number",
-                default: 1,
-            },
-            {
-                attribute: "radiusB",
-                type: "number",
-                default: 2,
-            },
-        ];
-    }
+  static fromAttributes(attributes: EllipseAttributes): Ellipse {
+    const { radiusA, radiusB } = attributes;
+    return new Ellipse(radiusA, radiusB);
+  }
+
+  get attributeData() {
+    return [
+      {
+        attribute: "radiusA",
+        type: "number",
+        default: 1,
+      },
+      {
+        attribute: "radiusB",
+        type: "number",
+        default: 2,
+      },
+    ];
+  }
 }
-
-/**
- * 
- * Example:
- * 
- * const ellipse = new Geometry.Ellipse(radiusA (aka height), radiusB (aka width))
- * 
- */
