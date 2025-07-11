df10e72edc827fb9ad378155cfb5793bc2382e6c
Devin Neal <devin@eulertour.com>
2025-07-11 11:00:49 -0400

    add manual loop example

--- html/common/navigation.js
+++ html/common/navigation.js
@@ -2,5 +2,6 @@ export const options = [
   { value: 'horizontal.html', text: 'Horizontal' },
   { value: 'vertical.html', text: 'Vertical' },
   { value: 'viewport-vertical.html', text: 'Viewport Vertical' },
-  { value: 'viewport-horizontal.html', text: 'Viewport Horizontal' }
+  { value: 'viewport-horizontal.html', text: 'Viewport Horizontal' },
+  { value: 'manual-loop.html', text: 'Manual Loop' },
 ];
