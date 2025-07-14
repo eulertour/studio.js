82f9d9842357b56266dde1ec74789e994282326f
Devin Neal <devin@eulertour.com>
2025-07-14 12:18:08 -0400

    update relative import to use vite alias

--- examples/scenes/civic101/utils.ts
+++ examples/scenes/civic101/utils.ts
@@ -1,4 +1,4 @@
-import { THREE } from "../../../src/index.js";
+import { THREE } from "@eulertour/studio";
 import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader.js";
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
